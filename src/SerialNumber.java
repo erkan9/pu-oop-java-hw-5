8285ba69508e778185fc975821506fda4828a7a6
Erkan Kamber <63397975+erkan9@users.noreply.github.com>
2021-02-22 21:32:40 +0200

    Homework 5

---
+++ src/SerialNumber.java
@@ -0,0 +1,40 @@
+import javax.swing.*;
+import java.awt.*;
+import java.util.Random;
+
+public class SerialNumber{
+
+    private final String randomSymbolHolder = "qwerty156asdfgh234klmnbz7890";
+
+    private String serialNumber;
+
+    char[] arrayForSymbols = new char[10];
+
+    Random random = new Random();
+
+    public SerialNumber() {}
+
+    public String getSerialNumber() {
+        return serialNumber;
+    }
+
+    //public void displaySerialNumber(Graphics g) {
+//
+    //    serialNumberSetter();
+    //    g.drawString(this.serialNumber, 400, 5);
+    //}
+
+    public String serialNumberSetter() {
+
+        int randomNumber;
+
+        for (int serialNumSymbolCounter = 0; serialNumSymbolCounter < 10; serialNumSymbolCounter++) {
+
+            randomNumber = this.random.nextInt(28);
+
+            this.arrayForSymbols[serialNumSymbolCounter] = this.randomSymbolHolder.charAt(randomNumber);
+        }
+
+        return this.serialNumber =  new String(this.arrayForSymbols);
+    }
+}
